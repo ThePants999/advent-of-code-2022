c7e49f02990ee69fd07196704cfcc7702ce06ac7
Chris Paterson <chris.paterson@microsoft.com>
2022-12-02 18:46:11 +0000

    Day 1

--- src/Day1.cs
+++ src/Day1.cs
@@ -1,18 +1,51 @@
 namespace Patersoft.AOC.AOC22;
 
+using Microsoft.Extensions.Logging;
+
 using Patersoft.AOC;
+using Patersoft.AOC.AOC22.Utils;
+
+using System.Collections.Generic;
 
 public class Day1 : Day
 {
-    public Day1 (AOCEnvironment env) : base("2022", 1, env) {}
+    List<int> elves = new List<int>();
+
+    public Day1(AOCEnvironment env) : base("2022", 1, env) { }
 
     protected override string ExecutePart1()
     {
-        return "foo";
+        int calories = 0;
+
+        foreach (string line in new LineReader(() => new StringReader(Input)))
+        {
+            if (line.Length == 0)
+            {
+                // Blank line - new Elf time
+                elves.Add(calories);
+                calories = 0;
+            } else {
+                try
+                {
+                    int newCals = Int32.Parse(line);
+                    calories += newCals;
+                }
+                catch (FormatException e)
+                {
+                    Env.Logger.LogError($"Invalid input: {line}", e);
+                    throw e;
+                }
+            }
+        }
+        elves.Add(calories);
+
+        elves.Sort();
+
+        return elves.Last<int>().ToString();
     }
 
     protected override string ExecutePart2()
     {
-        return "bar";
+        return (elves[elves.Count - 1] + elves[elves.Count - 2] + elves[elves.Count - 3]).ToString();
     }
 }

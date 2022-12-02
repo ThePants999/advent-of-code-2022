daa86d24a69d8e9f00c95a572be0fbc1fe48137d
Chris Paterson <chris.paterson@microsoft.com>
2022-12-02 18:20:35 +0000

    Initial skeleton, using https://github.com/ThePants999/advent-of-code-dotnet-runner as the framework

---
+++ src/Day1.cs
@@ -0,0 +1,18 @@
+namespace Patersoft.AOC.AOC22;
+
+using Patersoft.AOC;
+
+public class Day1 : Day
+{
+    public Day1 (AOCEnvironment env) : base("2022", 1, env) {}
+
+    protected override string ExecutePart1()
+    {
+        return "foo";
+    }
+
+    protected override string ExecutePart2()
+    {
+        return "bar";
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Patersoft.AOC.AOC22;
+
+using Patersoft.AOC;
+using Microsoft.Extensions.Logging;
+
+public class AOC22
+{
+    public static void Main(string[] args)
+    {
+        using ILoggerFactory loggerFactory =
+            LoggerFactory.Create(builder =>
+                builder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "HH:mm:ss ";
+                }).SetMinimumLevel(LogLevel.Warning));
+        ILogger<AOCRunner> logger = loggerFactory.CreateLogger<AOCRunner>();
+
+        AOCRunner runner = AOCRunner.BuildRunner(logger, new System.Type[] {
+            typeof(Day1)
+        });
+        runner.Run(1, 1);
+    }
+}

31f7b2b08398e331483d7515ec25a2d4280649c0
Chris Paterson <chris.paterson@microsoft.com>
2022-12-04 22:56:12 +0000

    Add a template Day class

---
+++ src/DayTemplate.cs
@@ -0,0 +1,21 @@
+namespace Patersoft.AOC.AOC22;
+
+using Patersoft.AOC;
+//using Patersoft.AOC.AOC22.Utils;
+//using System.Collections.Generic;
+//using System.Linq;
+
+public class DayX : Day
+{
+    public DayX(AOCEnvironment env) : base("2022", 0, env) { }
+
+    protected override string ExecutePart1()
+    {
+        return "";
+    }
+
+    protected override string ExecutePart2()
+    {
+        return "";
+    }
+}

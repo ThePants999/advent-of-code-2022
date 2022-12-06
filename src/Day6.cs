228f4ca06e978ea05c927dcf8726854ba3a8c369
Chris Paterson <chris.paterson@microsoft.com>
2022-12-06 06:23:08 +0000

    Day 6, yawn

--- src/Day6.cs
+++ src/Day6.cs
@@ -1,9 +1,7 @@
 namespace Patersoft.AOC.AOC22;
 
 using Patersoft.AOC;
-//using Patersoft.AOC.AOC22.Utils;
-//using System.Collections.Generic;
-//using System.Linq;
+using System.Collections.Generic;
 
 public class Day6 : Day
 {
@@ -11,26 +9,46 @@ public class Day6 : Day
 
     protected override string ExecutePart1()
     {
-        return "";
+        for (int index = 3; index < Input.Length; index++)
+        {
+            if (Input[index - 3] != Input[index - 2] &&
+                Input[index - 3] != Input[index - 1] &&
+                Input[index - 3] != Input[index] &&
+                Input[index - 2] != Input[index - 1] &&
+                Input[index - 2] != Input[index] &&
+                Input[index - 1] != Input[index])
+            {
+                return (index + 1).ToString();
+            }
+        }
+        return "error";
     }
 
     protected override string ExecutePart2()
     {
-        return "";
+        for (int index = 13; index < Input.Length; index++)
+        {
+            HashSet<char> set = new HashSet<char>(Input.Substring(index - 13, 14));
+            if (set.Count == 14)
+            {
+                return (index + 1).ToString();
+            }
+        }
+        return "error";
     }
 
     protected override string? GetExampleInput()
     {
-        return null;
+        return "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
     }
 
     protected override string? GetExamplePart1Answer()
     {
-        return null;
+        return "11";
     }
 
     protected override string? GetExamplePart2Answer()
     {
-        return null;
+        return "26";
     }
 }

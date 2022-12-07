392d199b133dac3aeb89fed8e1b5412101bb5db0
Chris Paterson <chris.paterson@microsoft.com>
2022-12-07 07:15:14 +0000

    Fix day 5 on Windows, sigh

--- src/Day5.cs
+++ src/Day5.cs
@@ -120,10 +120,12 @@ public class Day5 : Day
 
     protected override string? GetExampleInput()
     {
-        return @"    [D]    
-[N] [C]    
+        // Slightly odd structure here is because trailing space on lines 1 and 2
+        // is important and the editor wants to strip it :-/
+        return "    [D]    " + @"
+[N] [C]    " + @"
 [Z] [M] [P]
- 1   2   3 
+ 1   2   3
 
 move 1 from 2 to 1
 move 3 from 1 to 3

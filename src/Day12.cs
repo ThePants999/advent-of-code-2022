5589f75de90034961ff3725cfb7fc8c6e1dbf144
Chris Paterson <chris.paterson@microsoft.com>
2022-12-13 16:39:25 +0000

    Days 12 & 13

--- src/Day12.cs
+++ src/Day12.cs
@@ -1,36 +1,167 @@
 namespace Patersoft.AOC.AOC22;
 
 using Patersoft.AOC;
-//using Patersoft.AOC.AOC22.Utils;
-//using System.Collections.Generic;
-//using System.Linq;
+using Patersoft.AOC.AOC22.Utils;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Day12 : Day
 {
+    private Location[][]? _maze;
+    private Location? _top;
+
     public Day12(AOCEnvironment env) : base("2022", 12, env) { }
 
     protected override string ExecutePart1()
     {
-        return "";
+        Location start = CreateMaze();
+
+        BFS(start, loc => loc == _top!, (to, from) => to.Height - from.Height <= 1);
+
+        return _top!.Distance.ToString();
     }
 
     protected override string ExecutePart2()
     {
-        return "";
+        ResetMaze();
+
+        Location? bottom = BFS(_top!, loc => loc.Height == 0, (to, from) => from.Height - to.Height <= 1);
+
+        return bottom!.Distance.ToString();
+    }
+
+    private Location CreateMaze()
+    {
+        string fixedInput = Input.Replace("\r", null).Trim();
+        string[] lines = fixedInput.Split("\n");
+        int mazeHeight = lines.Length;
+        int mazeWidth = lines[0].Length;
+
+        Location? start = null;
+
+        _maze = new Location[mazeHeight][];
+        for (int row = 0; row < mazeHeight; row++)
+        {
+            _maze[row] = new Location[mazeWidth];
+            char[] chars = lines[row].ToCharArray();
+            for (int col = 0; col < mazeWidth; col++)
+            {
+                if (chars[col] == 'S')
+                {
+                    Location loc = new Location(row, col, 0);
+                    _maze[row][col] = loc;
+                    start = loc;
+                } else if (chars[col] == 'E') {
+                    Location loc = new Location(row, col, 25);
+                    _maze[row][col] = loc;
+                    _top = loc;
+                } else {
+                    _maze[row][col] = new Location(row, col, chars[col] - 'a');
+                }
+            }
+        }
+
+        return start!;
+    }
+
+    private void ResetMaze()
+    {
+        foreach (Location[] row in _maze!)
+        {
+            foreach (Location loc in row)
+            {
+                loc.Reset();
+            }
+        }
+    }
+
+    private Location? BFS(Location start, Predicate<Location> locationIsEnd, Func<Location, Location, bool> canReachFrom)
+    {
+        Queue<Location> q = new Queue<Location>();
+
+        start.Distance = 0;
+        start.Visited = true;
+        q.Enqueue(start);
+
+        while (q.Count > 0)
+        {
+            Location current = q.Dequeue();
+
+            List<Location> candidates = new List<Location>();
+            if (current.Row > 0)
+            {
+                candidates.Add(_maze![current.Row - 1][current.Col]);
+            }
+            if (current.Row < _maze!.Length - 1)
+            {
+                candidates.Add(_maze[current.Row + 1][current.Col]);
+            }
+            if (current.Col > 0)
+            {
+                candidates.Add(_maze[current.Row][current.Col - 1]);
+            }
+            if (current.Col < _maze[current.Row].Length - 1)
+            {
+                candidates.Add(_maze[current.Row][current.Col + 1]);
+            }
+
+            foreach (Location candidate in candidates)
+            {
+                if ((!candidate.Visited) && canReachFrom(candidate, current))
+                {
+                    candidate.Visited = true;
+                    candidate.Distance = current.Distance + 1;
+                    if (locationIsEnd(candidate))
+                    {
+                        return candidate;
+                    }
+                    q.Enqueue(candidate);
+                }
+            }
+        }
+
+        return null;
     }
 
     protected override string? GetExampleInput()
     {
-        return null;
+        return @"Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
     }
 
     protected override string? GetExamplePart1Answer()
     {
-        return null;
+        return "31";
     }
 
     protected override string? GetExamplePart2Answer()
     {
-        return null;
+        return "29";
+    }
+}
+
+internal class Location
+{
+    internal int Row { get; init; }
+    internal int Col { get; init; }
+    internal bool Visited { get; set; }
+    internal int Height { get; init; }
+    internal int Distance { get; set; }
+
+    internal Location(int row, int col, int height)
+    {
+        this.Row = row;
+        this.Col = col;
+        this.Height = height;
+        Reset();
+    }
+
+    internal void Reset()
+    {
+        Visited = false;
+        Distance = int.MaxValue;
     }
 }
--- src/Day13.cs
+++ src/Day13.cs
@@ -1,36 +1,252 @@
 namespace Patersoft.AOC.AOC22;
 
 using Patersoft.AOC;
-//using Patersoft.AOC.AOC22.Utils;
-//using System.Collections.Generic;
-//using System.Linq;
+using Patersoft.AOC.AOC22.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 public class Day13 : Day
 {
+    private List<PacketSection> _packets = new List<PacketSection>();
+
     public Day13(AOCEnvironment env) : base("2022", 13, env) { }
 
     protected override string ExecutePart1()
     {
-        return "";
+        foreach (string line in new LineReader(() => new StringReader(Input)))
+        {
+            if (line.Length == 0) { continue; }
+            int index = 1;
+            _packets.Add(PacketSection.Parse(line, ref index));
+        }
+
+        int indexSum = 0;
+        for (int i = 0; i < _packets.Count; i += 2)
+        {
+            if (_packets[i] <= _packets[i + 1])
+            {
+                indexSum += (i / 2) + 1;
+            }
+        }
+
+        return indexSum.ToString();
     }
 
     protected override string ExecutePart2()
     {
-        return "";
+        string divider = "[[2]]";
+        int index = 1;
+        PacketSection firstDivider = PacketSection.Parse(divider, ref index);
+        _packets.Add(firstDivider);
+        divider = "[[6]]";
+        index = 1;
+        PacketSection secondDivider = PacketSection.Parse(divider, ref index);
+        _packets.Add(secondDivider);
+
+        _packets.Sort();
+
+        int firstDividerLoc = _packets.IndexOf(firstDivider) + 1;
+        int secondDividerLoc = _packets.IndexOf(secondDivider) + 1;
+        return (firstDividerLoc * secondDividerLoc).ToString();
     }
 
     protected override string? GetExampleInput()
     {
-        return null;
+        return @"[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
     }
 
     protected override string? GetExamplePart1Answer()
     {
-        return null;
+        return "13";
     }
 
     protected override string? GetExamplePart2Answer()
     {
-        return null;
+        return "140";
+    }
+}
+
+internal class PacketSection : IComparable, IComparable<PacketSection>
+{
+    private int? IntValue { get; init; }
+    private List<PacketSection> _contents = new List<PacketSection>();
+
+    internal PacketSection(int intValue)
+    {
+        IntValue = intValue;
+    }
+
+    private PacketSection()
+    {
+        IntValue = null;
+    }
+
+    internal static PacketSection Parse(string input, ref int index)
+    {
+        PacketSection section = new PacketSection();
+        StringBuilder? itemSoFar = null;
+
+        while (index < input.Length)
+        {
+            int currentIndex = index;
+            index++;
+            if (input[currentIndex] == ']' || input[currentIndex] == ',')
+            {
+                // We're done with an item. If we have an in-progress item,
+                // it was an integer and we need to compile and add it.
+                // Otherwise, it was a list and the work is already done.
+                if (itemSoFar != null)
+                {
+                    int value = int.Parse(itemSoFar.ToString());
+                    itemSoFar = null;
+                    section._contents.Add(new PacketSection(value));
+                }
+
+                if (input[currentIndex] == ']')
+                {
+                    break;
+                }
+            }
+            else if (input[currentIndex] == '[')
+            {
+                section._contents.Add(Parse(input, ref index));
+            }
+            else if (input[currentIndex] == ',')
+            {
+                if (itemSoFar != null)
+                {
+                    int value = int.Parse(itemSoFar.ToString());
+                    itemSoFar = null;
+                    section._contents.Add(new PacketSection(value));
+                }
+                else
+                {
+                    // Current list item was a list. No-op.
+                }
+            }
+            else
+            {
+                if (itemSoFar == null)
+                {
+                    itemSoFar = new StringBuilder();
+                }
+                itemSoFar.Append(input[currentIndex]);
+            }
+        }
+
+        return section;
+    }
+
+    public int CompareTo(PacketSection? other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        if (this.IntValue.HasValue)
+        {
+            if (other.IntValue.HasValue)
+            {
+                // Compare two ints
+                return this.IntValue.Value.CompareTo(other.IntValue.Value);
+            }
+            else
+            {
+                // Compare int with list: convert int to list
+                PacketSection list = new PacketSection();
+                list._contents.Add(new PacketSection(this.IntValue.Value));
+                return list.CompareTo(other);
+            }
+        }
+        else
+        {
+            if (other.IntValue.HasValue)
+            {
+                // Compare list with int: convert int to list
+                PacketSection list = new PacketSection();
+                list._contents.Add(new PacketSection(other.IntValue.Value));
+                return this.CompareTo(list);
+            }
+            else
+            {
+                // Compare two lists
+                int index;
+                for (index = 0; index < this._contents.Count; index++)
+                {
+                    if (index >= other._contents.Count)
+                    {
+                        // This list is longer
+                        return 1;
+                    }
+
+                    int itemComparison = this._contents[index].CompareTo(other._contents[index]);
+                    if (itemComparison != 0)
+                    {
+                        return itemComparison;
+                    }
+                }
+
+                if (index < other._contents.Count)
+                {
+                    // Other list is longer
+                    return -1;
+                }
+                else
+                {
+                    // Lists match
+                    return 0;
+                }
+            }
+        }
+    }
+
+    public int CompareTo(object? obj)
+    {
+        PacketSection? other = obj as PacketSection;
+        return CompareTo(other);
+    }
+
+    public static bool operator <(PacketSection left, PacketSection right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator <=(PacketSection left, PacketSection right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >(PacketSection left, PacketSection right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    public static bool operator >=(PacketSection left, PacketSection right)
+    {
+        return left.CompareTo(right) >= 0;
     }
 }
